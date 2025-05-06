8899dc385e671f6a9609a9705cea936c1b297cb5
Lucas Storck <lucasstorck@icloud.com>
2025-05-06 14:30:34 -0300

    creation of the 'User' and 'UserResources' classes

---
+++ web-services/src/main/java/com/lucas/web_services/resources/UserResources.java
@@ -0,0 +1,13 @@
+package com.lucas.web_services.resources;
+
+import com.lucas.web_services.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+
+}
