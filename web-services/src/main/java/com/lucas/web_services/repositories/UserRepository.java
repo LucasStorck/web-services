e131e7239317cb917093058ffe21ca7132680bf7
Lucas Storck <lucasstorck@icloud.com>
2025-05-07 13:56:01 -0300

    creating test profile with h2 database

---
+++ web-services/src/main/java/com/lucas/web_services/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.web_services.repositories;
+
+import com.lucas.web_services.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
